f251ea64eccb94f8a5c717c32ddcdebbf2216133
juliolikescoding <jalfaro@techlaunch.io>
2019-02-05 21:22:45 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: Steven Widget
+Description: Widget that displays the title of the widget, the title of the site and a tagline for the website.
+Version:     0.1
+Author:    Steven Galvez
+
+*/
+
+
+class steven_Widget extends WP_Widget {
+  public function __construct() {
+    $widget_options = array( 'classname' => 'steven_widget', 'description' => 'This is a Simple Widget' );
+    parent::__construct( 'steven_widget', 'Steven Widget', $widget_options );
+  }
+  public function widget( $args, $instance ) {
+    $title = apply_filters( 'widget_title', $instance[ 'title' ] );
+    $blog_title = get_bloginfo( 'name' );
+    $tagline = get_bloginfo( 'description' );
+    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
+    <p><strong>Site Name:</strong> <?php echo $blog_title ?></p>
+    <p><strong>Tagline:</strong> <?php echo $tagline ?></p>
+    <?php echo $args['after_widget'];
+  }
+  
+  public function form( $instance ) {
+    $title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+      <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+    </p><?php
+  }
+  public function update( $new_instance, $old_instance ) {
+    $instance = $old_instance;
+    $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
+    return $instance;
+  }
+}
+function register_simple_widget() { 
+  register_widget( 'steven_widget' );
+}
+add_action( 'widgets_init', 'register_steven_widget' );
+?>
